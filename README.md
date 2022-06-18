40d357ff539645348df01471b0aa664a1a1c4400
Alisa.Kadilenko <alisa.kadilenko@nexign.com>
2022-06-19 00:09:05 +0300

    Add Slenide to JUnit test

---
+++ README.md
@@ -0,0 +1,5 @@
+# qa-guru-lesson-2-demo-qa
+
+Contains demoqa.com Student Registration Form tests:
+1. **StudentRegFormTest** (contains deprecated realization in case of any questions)
+2. **StudentRegFormTestWithPageObj** (contains realization with pageobjects)
---
+++ src/test/java/com/github/tests/SoftAssertionsGithub.java
@@ -0,0 +1,52 @@
+package com.github.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.*;
+
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class SoftAssertionsGithub {
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.holdBrowserOpen = true;
+        baseUrl = "https://github.com/selenide/selenide";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterAll
+    static void shutDown() {
+        closeWebDriver();
+    }
+
+    @Test
+    @DisplayName("Проверка примера кода для JUnit5 в SoftAssertions")
+    void checkJunitExamples() {
+        String linkText = "Soft assertions",
+                junitLocator = "#user-content-3-using-junit5-extend-test-class";
+
+        step("Открыть репозиторий Selenide на GitHub", ()->
+                open(baseUrl)
+        );
+        step("Перейти в Wiki", ()->
+                $("#wiki-tab").click()
+        );
+        step("Перейти в раздел " + linkText, ()->
+                $(linkText(linkText)).click()
+        );
+        step("Проверить наличие примера для JUnit5", ()->
+                $(junitLocator).shouldBe(visible)
+        );
+    }
+}
